3e26cfa4e1b8d0807c17cd30d6215b453cc4fcdc
Jinwoo Kim <kidjinwoo@me.com>
2022-10-17 10:15:51 +0900

    no message

--- scripts/build_cd_moms.sh
+++ scripts/build_cd_moms.sh
@@ -4,7 +4,6 @@ INPUT_DIR="common/sources/Entities"
 
 for model in $(find ${INPUT_DIR} -type d -name  "*.xcdatamodeld")
 do
-    $(xcode-select -p)/usr/bin/momc --sdkroot $(xcode-select -p)/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk --iphoneos-deployment-target 14.0 --module NamuTrackerApp ${model} ".theos/_/Applications/NamuTrackerApp.app"
     $(xcode-select -p)/usr/bin/momc --sdkroot $(xcode-select -p)/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk --iphoneos-deployment-target 14.0 --module NamuTrackerApp ${model} ".theos/_/Library/Application Support/NamuTracker" 
 done
 
